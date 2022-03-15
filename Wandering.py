f6141673a50d40db01e499296d97504d91511663
MissValen <valen2409s@gmail.com>
2022-03-15 15:02:10 -0500

    Realizamos cambios a wandering; derecha e izquierda

--- Wandering.py
+++ Wandering.py
@@ -2,13 +2,47 @@ import random
 
 class Wandering:
     
-    def __init__(self, name):
+    def __init__(self, name, x=0, y=0):
         self.name = name
-        
+        self.x = x
+        self.y = y
+    
+    def posicion(self):
+        return (self.x, self.y)
+    
+    def distance_origin(self):
+        return (self.x**2 + self.y**2)**0.5
+    
 class ComunWandering(Wandering):
     
     def __init__(self, name):
         super().__init__(name)
         
     def walk(self):
-        return random.choice([(0,2),(0,-2),(2,0),(-2,0)])
+        dx, dy = random.choice([(0,2),(0,-2),(2,0),(-2,0)])
+        self.x += dx
+        self.y += dy
+        return [dx, dy]
+    
+class RightWandering(Wandering):
+
+    def __init__(self, name):
+        super().__init__(name)
+        
+    def walk(self):
+        dx, dy = random.choice([(0,5),(5,0),(0,-1),(-1,0)])
+        self.x += dx
+        self.y += dy
+        return [dx, dy]
+    
+class LeftWandering(Wandering):
+
+    def __init__(self, name):
+        super().__init__(name)
+        
+    def walk(self):
+        dx, dy = random.choice([(0,5),(5,0),(0,-1),(-1,0)])
+        self.x += dx
+        self.y += dy
+        return [dx, dy]
+        
